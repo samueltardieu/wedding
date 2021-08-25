01cf0cc17c6a5044937bf1fa884d7d221ea37837
Ali Neishabouri <ali@neishabouri.net>
2021-08-25 19:59:39 +0200

    Add youtube background

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,12 @@
 import '../styles/index.scss';
 import 'bootstrap';
+import jQuery from "jquery";
+import "jquery.mb.ytplayer";
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
 console.log('webpack starterkit');
+
+jQuery('.player').YTPlayer();
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const Path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
@@ -23,6 +24,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html'),
     }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
   ],
   resolve: {
     alias: {
