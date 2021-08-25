97903cb5e3d820536e7b4931e62158da8ec6d31b
Ali Neishabouri <ali@neishabouri.net>
2021-08-25 20:29:34 +0200

    Add map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,26 @@ import '../styles/index.scss';
 import 'bootstrap';
 import jQuery from "jquery";
 import "jquery.mb.ytplayer";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+jQuery(document).ready(function () {
+  jQuery('.player').YTPlayer();
 
-jQuery('.player').YTPlayer();
+  var map = L.map('map-canvas', {
+    zoomControl: true,
+    scrollWheelZoom: false
+  }).setView([47.8561764, -2.2697361], 8);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([47.8561764, -2.2697361]).addTo(map)
+    .bindPopup('Manoir de la Fresnay.')
+    .openPopup();
+});
