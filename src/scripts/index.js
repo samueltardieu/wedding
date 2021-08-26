f91b0242bd32054a269675144a9537f452976f15
Ali Neishabouri <ali@neishabouri.net>
2021-08-26 15:02:36 +0200

    Fix leaflet labels
    Workaround found here https://github.com/PaulLeCam/react-leaflet/issues/255

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,19 @@ import "jquery.mb.ytplayer";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// stupid hack so that leaflet's images work after going through webpack
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: marker2x,
+    iconUrl: marker,
+    shadowUrl: markerShadow
+});
+
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
